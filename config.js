51b5950d433340717fbac37956bf6b7b51ef6ffd
Thot-null <3277655825@qq.com>
2023-07-08 13:30:53 +0800

    第一次提交文件

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '半步多的站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2209099-00742844520abd81096a2d2d'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://i.thotz.top/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/thot-null'
+    },
+    {
+      text: '博客',
+      url: 'https://thotz.top/'
+    },
+  ],
+};
